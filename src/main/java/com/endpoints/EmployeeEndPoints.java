591bec7ea96760a5680e1b0c572e49d940cf00b1
vikspwr <pawar.vikas1111>
2024-02-09 23:41:13 +0530

    added crud ops

--- src/main/java/com/endpoints/EmployeeEndPoints.java
+++ src/main/java/com/endpoints/EmployeeEndPoints.java
@@ -4,35 +4,54 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import com.payload.EmployeeData;
 import com.routes.Routes;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class EmployeeEndPoints {
-	
+
 	public static Response fetchAllEmployee() {
-		
-		Response response = given()
-			.contentType(ContentType.JSON)
-		
-		.when()
-			.get(Routes.fetchAllEmployee);
-		
+
+		Response response = given().contentType(ContentType.JSON)
+
+				.when().get(Routes.fetchAllEmployee);
+
 		return response;
 	}
-	
+
 	public static Response fetchSingleEmployee(String id) {
-		
-		Response response = given()
-					.contentType(ContentType.JSON)
-					.pathParam("empId", id)
-		.when()
-			.get(Routes.fetchSingleEmployee);
-		
+
+		Response response = given().contentType(ContentType.JSON).pathParam("empId", id).when()
+				.get(Routes.fetchSingleEmployee);
+
 		return response;
-		
+
 	}
-	
+
+	public static Response createEmployee(EmployeeData data) {
+
+		Response response = given().contentType(ContentType.JSON).body(data).when().post(Routes.createNewEmployee);
+		return response;
+	}
+
+	public static Response deleteEmployee(String id) {
+
+		Response response = given().contentType(ContentType.JSON).pathParam("empId", id).when()
+				.delete(Routes.deleteExistingEmployee);
+
+		return response;
+
+	}
+
+	public static Response updateEmployee(EmployeeData data, String id) {
+
+		Response response = given().contentType(ContentType.JSON).pathParam("empId", id).body(data).when()
+				.put(Routes.updateExistingEmployee);
+
+		return response;
+
+	}
 
 }
--- src/test/java/com/testcases/TestEmployeeEndPoints.java
+++ src/test/java/com/testcases/TestEmployeeEndPoints.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.endpoints.EmployeeEndPoints;
+import com.payload.EmployeeData;
 
 import io.restassured.response.Response;
 
@@ -15,20 +17,84 @@ public class TestEmployeeEndPoints {
 
 	@Test(priority = 1)
 	public void verifyFetchAllEmployee() {
-		
+
 		Response response = EmployeeEndPoints.fetchAllEmployee();
 		Assert.assertEquals(response.getStatusCode(), 200);
-		
+
+		response.then().log().body();
+
 	}
-	
+
 	@Test(priority = 2)
 	public void verifyFetchSingleEmployee() {
-		
+		SoftAssert sa = new SoftAssert();
 		Response response = EmployeeEndPoints.fetchSingleEmployee("1");
 		response.then().log().body();
+		sa.assertEquals(response.getStatusCode(), 200);
+		String name = response.jsonPath().get("name").toString();
+		sa.assertEquals(name, "Vikas");
+		sa.assertAll();
+	}
+
+	@Test(priority = 3)
+	public void verifyCreateEmployee() {
+		EmployeeData data = new EmployeeData();
+
+		String[] courses = { "Java", "Selenium" };
+		data.setId("101");
+		data.setName("xyz");
+		data.setMarried("married");
+		data.setCompany("cname");
+		data.setSalary("60000");
+		data.setCourses(courses);
+		data.setAddress("Ngn");
+
+		Response response = EmployeeEndPoints.createEmployee(data);
+
+		Assert.assertEquals(response.getStatusCode(), 201);
+
+	}
+
+	@Test(priority = 5)
+	public void verifyDeleteEmployee() {
 		
+		Response response = EmployeeEndPoints.deleteEmployee("0d6a");
+		Assert.assertEquals(response.getStatusCode(), 200);
 	}
 	
-	
+	/*
+	 * {
+        "id": "a7ee"
+    },
+    {
+        "id": "17d0"
+    },
+    {
+        "id": "984e"
+    },
+    {
+        "id": "0d6a"
+    },
+	 * 
+	 */
 
+	@Test(priority = 4)
+	public void verifyUpdateEmployee() {
+		
+		EmployeeData data = new EmployeeData();
+
+		String[] courses = { "OOps", "Selenium" };
+		
+		data.setName("xyzupdated");
+		data.setMarried("marriedupdated");
+		data.setCompany("cnameupdated");
+		data.setSalary("70000");
+		data.setCourses(courses);
+		data.setAddress("Pune");
+
+		Response response = EmployeeEndPoints.updateEmployee(data, "101");
+
+		Assert.assertEquals(response.getStatusCode(), 200);
+
+	}
 }
